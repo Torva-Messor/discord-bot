be47e9c821d09db4d06cb950146a599b799f0d78
alik-6 <authastefard@proton.me>
2026-01-31 20:54:22 +0500

    removed subcommands on plugin group, refactoring it to  `hybrid_command`

--- plugins/dev/main.py
+++ plugins/dev/main.py
@@ -29,36 +29,9 @@ class Dev(commands.Cog):
         else:
             await ctx.reply(f"Synced {len(spec)} commands to this guild.")
 
-    @commands.hybrid_group(name='plugin')
+    @commands.hybrid_command(name='plugin')
     async def plugin(self, ctx: commands.Context[Bot]):
         await ctx.reply(view=PluginView(bot=self.bot))
-
-    @plugin.command('list')
-    async def plugins_list(self, ctx: commands.Context[Bot]):
-        embed = Embed(title="Plugin List")
-        for index, key in enumerate(self.bot.extensions.keys()):
-            plugin_name = key.split('.')[-2]
-            embed.add_field(name=str(index + 1), value=plugin_name, inline=index % 3 == 0 if False else True)
-        await ctx.reply(embeds=[embed])
-
-    async def plugin_autocomplete(self, interaction: Interaction, current: str):
-        choices = []
-        for key in self.bot.extensions.keys():
-            display_name = key.split('.')[-2] 
-            if current.lower() in display_name.lower():
-                choices.append(app_commands.Choice(name=display_name, value=key))
-        
-        # Return only the first 25 [Discord limit]
-        return choices[:25]
-
-    @plugin.command('reload')
-    @app_commands.autocomplete(plugin=plugin_autocomplete)
-    async def plugin_reload(self, ctx: commands.Context[Bot], plugin: str):
-        try:
-            await self.bot.reload_extension(plugin)
-            await ctx.reply(f'Plugin `{plugin}` reloaded')
-        except Exception as e:
-            await ctx.reply(str(e))
 
 async def setup(bot: Bot):
     """
